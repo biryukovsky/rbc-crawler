0ebb106f0b651694f985aa78dd868c1dc6fb045f
Ilya Biryukov <dev.biryukovsky@gmail.com>
2018-12-15 01:47:49 +0300

    collect news data

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import time
+from urllib.parse import urlparse
 import requests
 from fake_useragent import UserAgent
 from bs4 import BeautifulSoup
 
 
-URL = 'https://www.rbc.ru/'
+MAIN_URL = 'https://www.rbc.ru/'
 ua = UserAgent()
 
 
@@ -18,9 +20,47 @@ def get_news_links(url):
 
     # get first big link
     links.append(soup.find('a', class_='main__big').attrs['href'])
-    links_container = soup.find('div', class_='main-feed')
 
+    links_container = soup.find('div', class_='main-feed')
     for link in links_container.find_all('a', class_='main-feed__link'):
         links.append(link.attrs['href'])
 
     return links
+
+
+def get_news_content(links):
+    content = []
+    for link in links:
+        news_html = get_html(link, headers={'User-Agent': ua.random})
+        soup = BeautifulSoup(news_html, 'html.parser')
+        title = soup.find('div', attrs={
+            "class": "article__header"
+        }).find('div', attrs={
+            "class": "article__header__title"
+        }).text
+
+        article_content = soup.find('div', class_='article__content')
+        body = ''
+        for p in article_content.find_all('p'):
+            body += p.text
+        
+        path = urlparse(link).path.split('/')
+        date = list(filter(lambda e: e.isdigit(), path)) or ''
+
+        content.append({
+            "title": title,
+            'link': link,
+            "date": '.'.join(date),
+            'body': body
+        })
+
+        # Thanks robots.txt
+        time.sleep(1)
+
+    return content
+
+
+if __name__ == '__main__':
+    links = get_news_links(MAIN_URL)
+    time.sleep(1)
+    content = get_news_content(links)
